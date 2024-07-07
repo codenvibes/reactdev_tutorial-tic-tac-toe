bb6b2ee1a633f296458f797b8d3a3251583ef872
codenvibes <terrymk1999@gmail.com>
2024-07-07 15:27:09 +0300

    Update Square component to accept a value prop and display different values on the board

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 /**
  * FILE: App.js
  */
-function Square() {
-  return <button classname="square">1</button>
+function Square({ value }) {
+  return <button classname="square">{value}</button>
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
       </div>
     </>
   );

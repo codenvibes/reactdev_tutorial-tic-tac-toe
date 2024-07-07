59618b63b110e7900f3bf14f331acbeba3c4f01e
codenvibes <terrymk1999@gmail.com>
2024-07-07 15:05:46 +0300

    Refactor: Organize buttons into board rows and update button labels in Square component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+/**
+ * FILE: App.js
+ */
 export default function Square() {
   return (
     <>
-      <button classname="square">X</button>
-      <button classname="square">X</button>
-      <button classname="square">X</button>
-      <button classname="square">X</button>
-      <button classname="square">X</button>
-      <button classname="square">X</button>
-      <button classname="square">X</button>
-      <button classname="square">X</button>
-      <button classname="square">X</button>
+      <div className="board-row">
+        <button classname="square">1</button>
+        <button classname="square">2</button>
+        <button classname="square">3</button>
+      </div>
+      <div className="board-row">
+        <button classname="square">4</button>
+        <button classname="square">5</button>
+        <button classname="square">6</button>
+      </div>
+      <div className="board-row">
+        <button classname="square">7</button>
+        <button classname="square">8</button>
+        <button classname="square">9</button>
+      </div>
     </>
   );
 }

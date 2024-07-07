84428ed072a6a7111887177ae002457d3acdb8f5
codenvibes <terrymk1999@gmail.com>
2024-07-07 13:04:39 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+export default function Square() {
+  return (
+    <>
+      <button classname="square">X</button>
+      <button classname="square">X</button>
+      <button classname="square">X</button>
+      <button classname="square">X</button>
+      <button classname="square">X</button>
+      <button classname="square">X</button>
+      <button classname="square">X</button>
+      <button classname="square">X</button>
+      <button classname="square">X</button>
+    </>
+  );
+}
